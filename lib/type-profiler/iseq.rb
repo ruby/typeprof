2c536b50dd29adec4036c0b31d1aa695d078fb0a
Yusuke Endoh <mame@ruby-lang.org>
2019-04-02 17:39:00 +0900

    Remove unused escape analysis

--- lib/type-profiler/iseq.rb
+++ lib/type-profiler/iseq.rb
@@ -9,9 +9,7 @@ module TypeProfiler
       opt[:specialized_instruction] = false
       opt[:operands_unification] = false
       opt[:coverage_enabled] = false
-      iseq = new(RubyVM::InstructionSequence.compile_file(file, opt).to_a)
-      iseq.escape_analysis
-      iseq
+      new(RubyVM::InstructionSequence.compile_file(file, opt).to_a)
     end
 
     def self.compile_str(str)
@@ -21,17 +19,13 @@ module TypeProfiler
       opt[:specialized_instruction] = false
       opt[:operands_unification] = false
       opt[:coverage_enabled] = false
-      iseq = new(RubyVM::InstructionSequence.compile(str, opt).to_a)
-      iseq.escape_analysis
-      iseq
+      new(RubyVM::InstructionSequence.compile(str, opt).to_a)
     end
 
     def initialize(iseq)
       _magic, _major_version, _minor_version, _format_type, _misc,
         @name, @path, @absolute_path, @start_lineno, @type,
         @locals, @args, _catch_table, insns = *iseq
-
-      @escaped_locals = []
 
       i = 0
       labels = {}
@@ -88,45 +82,10 @@ module TypeProfiler
       end
     end
 
-    def escape_analysis
-      identify_escaped_locals([])
-      replace_local_access_insns([])
-    end
-
-    def identify_escaped_locals(parent_iseqs)
-      @insns.each do |insn, *operands|
-        operands.each do |operand|
-          operand.identify_escaped_locals(parent_iseqs + [self]) if operand.is_a?(ISeq)
-        end
-        if insn == :setlocal
-          var_idx, scope_idx = operands
-          if scope_idx >= 1
-            tiseq = parent_iseqs[-scope_idx]
-            tiseq.escaped_locals << var_idx
-          end
-        end
-      end
-    end
-
-    def replace_local_access_insns(parent_iseqs)
-      @insns = @insns.map do |insn, *operands|
-        operands.each do |operand|
-          operand.replace_local_access_insns(parent_iseqs + [self]) if operand.is_a?(ISeq)
-        end
-        if [:getlocal, :getblockparam, :getblockparamproxy, :setlocal].include?(insn)
-          var_idx, scope_idx = operands
-          tiseq = (parent_iseqs + [self])[-scope_idx - 1]
-          [insn, *operands, tiseq.escaped_locals.include?(var_idx)]
-        else
-          [insn, *operands]
-        end
-      end
-    end
-
     def source_location(pc)
       "#{ @path }:#{ @linenos[pc] }"
     end
 
-    attr_reader :name, :path, :abolute_path, :start_lineno, :type, :locals, :escaped_locals, :args, :insns, :linenos
+    attr_reader :name, :path, :abolute_path, :start_lineno, :type, :locals, :args, :insns, :linenos
   end
 end
