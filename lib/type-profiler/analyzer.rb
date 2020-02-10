3cc555ba9678aeece78708c1afa793eaff6a8c67
Yusuke Endoh <mame@ruby-lang.org>
2020-02-10 12:23:15 +0900

    Refactoring

--- lib/type-profiler/analyzer.rb
+++ lib/type-profiler/analyzer.rb
@@ -107,12 +107,9 @@ module TypeProfiler
 
     def push(*tys)
       tys.each do |ty|
-        raise "Array cannot be pushed to the stack" if ty.is_a?(Type::Array)
         raise "nil cannot be pushed to the stack" if ty.nil?
-        if ty.is_a?(Type::Union)
-          ty.each_child do |ty|
-            raise "Array (in Union type) cannot be pushed to the stack" if ty.is_a?(Type::Array)
-          end
+        ty.each_child do |ty|
+          raise "Array cannot be pushed to the stack" if ty.is_a?(Type::Array)
         end
       end
       Env.new(@recv_ty, @blk_ty, @locals, @stack + tys, @type_params)
