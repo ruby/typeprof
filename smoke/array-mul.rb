73120a5d7ca683129ff36adb67a741b704e9db0c
Yusuke Endoh <mame@ruby-lang.org>
2020-07-06 17:08:13 +0900

    Add a smoke

---
+++ smoke/array-mul.rb
@@ -0,0 +1,17 @@
+def foo(a)
+  a * 3
+end
+
+def bar(a)
+  a * "join"
+end
+
+foo([1, 2, 3])
+bar([1, 2, 3])
+
+__END__
+# Classes
+class Object
+  def foo : ([Integer, Integer, Integer]) -> Array[Integer]
+  def bar : ([Integer, Integer, Integer]) -> String
+end
