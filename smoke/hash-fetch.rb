41ed73ead55854fa91bf44ad579d0d4802e07748
Yusuke Endoh <mame@ruby-lang.org>
2020-08-04 16:37:02 +0900

    Forgot to add a smoke

---
+++ smoke/hash-fetch.rb
@@ -0,0 +1,27 @@
+h = { a: :A, b: :B }
+
+def foo(h)
+  h.fetch(:a)     #=> :A | :B
+end
+def bar(h)
+  h.fetch(:a, :C) #=> :A | :B | :C
+end
+def baz(h)
+  n = nil
+  [h.fetch(:a) do |k| #=> :A | :B | :C
+    n = k #=> :A | :B
+    :C
+  end, n]
+end
+
+foo(h)
+bar(h)
+baz(h)
+
+__END__
+# Classes
+class Object
+  def foo : ({:a=>:A, :b=>:B}) -> (:A | :B)
+  def bar : ({:a=>:A, :b=>:B}) -> (:A | :B | :C)
+  def baz : ({:a=>:A, :b=>:B}) -> ([:A | :B | :C, :a | NilClass])
+end
