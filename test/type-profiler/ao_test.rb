87652d7f89fe896fe006c54fa1de97f47564bb85
Yusuke Endoh <mame@ruby-lang.org>
2020-09-11 00:09:03 +0900

    Add a test for testbed/ao.rb

---
+++ test/type-profiler/ao_test.rb
@@ -0,0 +1,73 @@
+require_relative "test_helper"
+require_relative "../../lib/type-profiler"
+
+module TypeProfiler
+  class AOBenchTest < Test::Unit::TestCase
+    test "testbed/ao.rb" do
+      path = File.join(__dir__, "../../testbed/ao.rb")
+      name = "testbed/ao.rb"
+
+      actual = TestRun.run(name, File.read(path))
+
+      expected = <<-END
+# Classes
+class Object
+  def clamp : (Float) -> Integer
+  def otherBasis : (Vec) -> [Vec, Vec, Vec]
+  def top : -> Integer
+end
+
+class Vec
+  attr_accessor x : Float
+  attr_accessor y : Float
+  attr_accessor z : Float
+  def initialize : (Float, Float, Float) -> Float
+  def vadd : (Vec) -> Vec
+  def vsub : (Vec) -> Vec
+  def vcross : (Vec) -> Vec
+  def vdot : (Vec) -> Float
+  def vlength : -> Float
+  def vnormalize : -> Vec
+end
+
+class Sphere
+  attr_reader center : Vec
+  attr_reader radius : Float
+  def initialize : (Vec, Float) -> Float
+  def intersect : (Ray, Isect) -> Vec?
+end
+
+class Plane
+  @p : Vec
+  @n : Vec
+  def initialize : (Vec, Vec) -> Vec
+  def intersect : (Ray, Isect) -> Vec?
+end
+
+class Ray
+  attr_accessor org : Vec
+  attr_accessor dir : Vec
+  def initialize : (Vec, Vec) -> Vec
+end
+
+class Isect
+  attr_accessor t : Float
+  attr_accessor hit : bool
+  attr_accessor pl : Vec
+  attr_accessor n : Vec
+  def initialize : -> Vec
+end
+
+class Scene
+  @spheres : [Sphere, Sphere, Sphere]
+  @plane : Plane
+  def initialize : -> Plane
+  def ambient_occlusion : (Isect) -> Vec
+  def render : (Integer, Integer, Integer) -> Integer
+end
+      END
+
+      assert_equal(expected, actual)
+    end
+  end
+end
--- test/type-profiler/test_helper.rb
+++ test/type-profiler/test_helper.rb
@@ -4,3 +4,48 @@ SimpleCov.start do
 end
 
 require "test-unit"
+
+module TypeProfiler
+  class TestRun
+    class DummyStdout
+      def initialize
+        @buffer = []
+      end
+
+      def puts(s = nil)
+        @buffer << (s ? s + "\n" : "\n")
+      end
+
+      def result
+        @buffer.join
+      end
+    end
+
+    def self.run(name, code)
+      new(name, code).run
+    end
+
+    def initialize(name, code)
+      @name, @code = name, code
+    end
+
+    def run
+      ENV["TP_SHOW_ERRORS"] = "1"
+      ENV["TP_DETAILED_STUB"] = "1"
+      $output = []
+      verbose_back, $VERBOSE = $VERBOSE, nil
+
+      iseq = TypeProfiler::ISeq.compile_str(@code, @name)
+      buffer = DummyStdout.new
+      TypeProfiler.type_profile(iseq, nil, buffer)
+
+      buffer.result
+
+    ensure
+      ENV.delete("TP_SHOW_ERRORS")
+      ENV.delete("TP_DETAILED_STUB")
+      $output = nil
+      $VERBOSE = verbose_back
+    end
+  end
+end
