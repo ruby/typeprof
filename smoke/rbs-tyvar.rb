b22bdc7397138855dbff7bf98ca81baa6905e343
Yusuke Endoh <mame@ruby-lang.org>
2020-10-23 17:45:19 +0900

    Add smoke tests for RBS tyvar

---
+++ smoke/rbs-tyvar.rb
@@ -0,0 +1,18 @@
+def foo
+  cell = Cell.new("str")
+  cell.set_elem(42) if rand < 0.5
+  cell
+end
+
+def bar
+  foo.get_elem
+end
+
+bar
+
+__END__
+# Classes
+class Object
+  def foo : -> (Cell[Integer | String])
+  def bar : -> (Integer | String)
+end
