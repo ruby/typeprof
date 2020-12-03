34fc440aa874b0eac7e096471cb39325ff745ece
Yusuke Endoh <mame@ruby-lang.org>
2020-12-04 01:13:48 +0900

    Fix smoke test

--- smoke/symbol-proc-attr.rb
+++ smoke/symbol-proc-attr.rb
@@ -8,3 +8,15 @@ end
 def log
   [Foo.new(42)].map(&:foo)
 end
+
+__END__
+# Classes
+class Object
+  private
+  def log : -> (Array[Integer | untyped])
+end
+
+class Foo
+  attr_reader foo : Integer | untyped
+  def initialize : (Integer | untyped) -> (Integer | untyped)
+end
