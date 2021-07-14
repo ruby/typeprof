737d5ab5a0a2c5b137c2825192aa4a6880d34363
Yusuke Endoh <mame@ruby-lang.org>
2021-07-14 16:58:19 +0900

    Start experimenting with RBSWiki

--- lib/typeprof/analyzer.rb
+++ lib/typeprof/analyzer.rb
@@ -1047,7 +1047,6 @@ module TypeProf
         [ep&.detailed_source_location, msg]
       end
 
-      pp errs
       res = RubySignatureExporter.new(self, @class_defs, @iseq_method_to_ctxs).show_lsp
       {
         sigs: res,
