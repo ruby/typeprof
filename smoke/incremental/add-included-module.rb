06f001510961e74c6c05a53ce9b02eb2774c95c7
Yusuke Endoh <mame@ruby-lang.org>
2023-03-31 17:27:44 +0900

    add a test

---
+++ smoke/incremental/add-included-module.rb
@@ -0,0 +1,37 @@
+# update: test0.rb
+module M
+end
+
+def foo
+  M::A.new(1)
+end
+# update: test0.rb
+module M
+end
+
+def foo
+  M::A.new(1)
+end
+
+# update: test1.rb
+module M
+  class A
+    def initialize(n)
+    end
+  end
+end
+
+# update: test1.rb
+module M
+  class A
+    def initialize(n)
+    end
+  end
+end
+
+# assert: test1.rb
+module M
+  class M::A
+    def initialize: (Integer) -> nil
+  end
+end
