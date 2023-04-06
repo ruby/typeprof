3d2f46cf7f610177d3d500e9db564405c6abb33c
Yusuke Endoh <mame@ruby-lang.org>
2023-04-06 11:52:25 +0900

    the scope of type aliases

---
+++ smoke/rbs/type-alias.rb
@@ -0,0 +1,53 @@
+# update: test.rbs
+type a = Integer
+class Foo
+  type a = Integer | String
+  def foo: (a) -> a
+end
+
+# update: test.rb
+def test1
+  Foo.new.foo(1)
+end
+
+def test2
+  Foo.new.foo(1.0)
+end
+
+def test3(unknown)
+  Foo.new.foo(unknown)
+end
+
+# assert: test.rb
+class Object
+  def test1: -> (Integer | String)
+  def test2: -> untyped
+  def test3: (untyped) -> untyped
+end
+
+# update: test.rbs
+type a = Integer
+class Foo
+  type a = Integer | String
+  def foo: (::a) -> ::a
+end
+
+# assert: test.rb
+class Object
+  def test1: -> (Integer | String)
+  def test2: -> untyped
+  def test3: (untyped) -> untyped
+end
+
+# update: test.rbs
+type a = Integer
+class Foo
+  def foo: (a) -> a
+end
+
+# assert: test.rb
+class Object
+  def test1: -> Integer
+  def test2: -> untyped
+  def test3: (untyped) -> untyped
+end
