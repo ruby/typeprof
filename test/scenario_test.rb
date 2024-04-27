603d5d47016e62b69cfe6503a444c743716ec784
Yusuke Endoh <mame@ruby-lang.org>
2024-04-27 13:57:23 +0900

    scenario/known-issues

--- test/scenario_test.rb
+++ test/scenario_test.rb
@@ -4,5 +4,6 @@ require_relative "scenario_compiler"
 slow = ENV["SLOW"]
 base_dir = File.join(File.dirname(__dir__))
 scenarios = Dir.glob(File.join(base_dir, "scenario/**/*.rb"))
+scenarios = scenarios.reject {|path| path.start_with?(File.join(base_dir, "scenario/known-issues")) }
 scenarios = scenarios.map {|scenario| scenario.delete_prefix(base_dir + File::Separator) }
 ScenarioCompiler.new(scenarios, interactive: false, fast: !slow).run
