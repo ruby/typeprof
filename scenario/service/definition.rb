4449ce0dcbe566f84fb458467026c6eb02649ebd
Shia <rise.shia@gmail.com>
2024-04-30 07:36:16 +0900

    Add GoToDefinition tests for CONST, attr_reader

--- scenario/service/definition.rb
+++ scenario/service/definition.rb
@@ -1,6 +1,11 @@
 ## update: test.rb
 class Foo
+  BAR = 1
+
+  attr_reader :bar, :baz
+
   def initialize(n)
+    @bar = n
   end
 
   def foo(n)
@@ -8,12 +13,24 @@ class Foo
 end
 
 Foo.new(1).foo(1.0)
+Foo.new(1).bar
+Foo.new(1).baz
+Foo::BAR
 
-## definition: test.rb:9:1
-test.rb:(1,0)-(7,3)
+## definition: test.rb:14:1
+test.rb:(1,0)-(12,3)
 
-## definition: test.rb:9:5
-test.rb:(2,6)-(2,16)
+## definition: test.rb:14:5
+test.rb:(6,6)-(6,16)
 
-## definition: test.rb:9:12
-test.rb:(5,6)-(5,9)
+## definition: test.rb:14:12
+test.rb:(10,6)-(10,9)
+
+## definition: test.rb:15:12
+test.rb:(4,2)-(4,24)
+
+## definition: test.rb:16:12
+test.rb:(4,2)-(4,24)
+
+## definition: test.rb:17:5
+test.rb:(2,2)-(2,9)
