8e20e4eb2c71512e6ebd2ec74e422efe32d3e2b1
Yusuke Endoh <mame@ruby-lang.org>
2024-04-30 22:59:25 +0900

    Refactor scenario runner

--- tool/scenario_runner.rb
+++ tool/scenario_runner.rb
@@ -1,3 +1,3 @@
 require_relative "../test/scenario_compiler"
 
-ScenarioCompiler.new(ARGV[0] ? ARGV : ["t.rb"], interactive: false).run
+ScenarioCompiler.new(ARGV[0] ? ARGV : ["t.rb"], output_declarations: true, check_diff: false).run
