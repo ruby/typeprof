231f663575b19d3b8c929012aff0ced1526bc7f6
Shia <rise.shia@gmail.com>
2024-05-02 16:22:16 +0900

    Add comment to service/definition scenario test

--- scenario/service/definition.rb
+++ scenario/service/definition.rb
@@ -23,28 +23,28 @@ Foo::CCAR
 Foo::DDAR
 
 ## definition: test.rb:14:1
-test.rb:(1,6)-(1,9)
+test.rb:(1,6)-(1,9) # Jump to Foo class
 
 ## definition: test.rb:14:5
-test.rb:(6,6)-(6,16)
+test.rb:(6,6)-(6,16) # Jump Foo.initialize from Foo.new
 
 ## definition: test.rb:14:12
-test.rb:(10,6)-(10,9)
+test.rb:(10,6)-(10,9) # Jump to Foo#foo
 
 ## definition: test.rb:15:12
-test.rb:(4,2)-(4,24)
+test.rb:(4,2)-(4,24) # Jump to Foo#bar (first arg of attr_reader)
 
 ## definition: test.rb:16:12
-test.rb:(4,2)-(4,24)
+test.rb:(4,2)-(4,24) # Jump to Foo#baz (second arg of attr_reader)
 
 ## definition: test.rb:17:5
-test.rb:(2,2)-(2,5)
+test.rb:(2,2)-(2,5) # Jump to Foo#BAR (constant_write_node)
 
 ## definition: test.rb:19:5
-test.rb:(18,0)-(18,9)
+test.rb:(18,0)-(18,9) # Jump to Foo#BBAR (constant_path_write_node)
 
 ## definition: test.rb:21:5
-test.rb:(20,0)-(20,9)
+test.rb:(20,0)-(20,9) # Jump to Foo#BBAR (first arg of constant_path_target_node)
 
 ## definition: test.rb:22:5
-test.rb:(20,11)-(20,20)
+test.rb:(20,11)-(20,20) # Jump to Foo#BBAR (second arg of constant_path_target_node)
