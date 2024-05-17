123bc53d37f698b8c121bbb1aac54e342444c89d
Masato Sugiyama <public@smasato.net>
2024-05-17 10:36:45 +0900

    Add bundler setup to scenario runner

--- tool/scenario_runner.rb
+++ tool/scenario_runner.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+require "bundler/setup"
+
 require_relative "../test/scenario_compiler"
 
 ScenarioCompiler.new(ARGV[0] ? ARGV : ["t.rb"], output_declarations: true, check_diff: false).run
