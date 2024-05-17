431a4e8c8ad868e4bdfbdb5e743c2087557c0840
hiromi-ogawa <hiromi-ogawa@cookpad.com>
2024-05-17 14:48:01 +0900

    Add rescue_assign scenario to known-issues

---
+++ scenario/known-issues/rescue_assign.rb
@@ -0,0 +1,16 @@
+## update
+def foo(n)
+  raise if n != 0
+  n.to_s
+rescue StandardError => e
+  e.message
+end
+
+foo(1)
+
+## diagnostics
+
+## assert
+class Object
+  def foo: (Integer) -> String?
+end
