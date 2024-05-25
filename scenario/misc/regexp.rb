cd61f384260c01255c16bf1691541b3d68ff1f40
alpaca-tc <alpaca-tc@alpaca.tc>
2024-05-25 23:11:01 +0900

    Support match_last_line_node and interpolated_match_last_line_node
    In prism, conditional statements that are passed only RegExp is parsed as `MatchLastLineNode`/`InterpolatedMatchLastLineNode`.
    related: https://github.com/ruby/prism/pull/1442
    
    Those nodes are now supported in this PR.
    
    We almost never use the `$LAST_READ_LINE` + `if RegExp` feature, but in the process of editing the code, it may be parsed to those node.

--- scenario/misc/regexp.rb
+++ scenario/misc/regexp.rb
@@ -5,9 +5,17 @@ end
 def check2
   /foo#{ 1 }bar/
 end
+def check3
+  if /foo/ then end
+end
+def check4
+  if /foo#{ 1 }/ then end
+end
 
 ## assert
 class Object
   def check1: -> Regexp
   def check2: -> Regexp
+  def check3: -> nil
+  def check4: -> nil
 end
