e9601e68a4be8501b28fe358dde0c2d111c505bd
Yusuke Endoh <mame@ruby-lang.org>
2024-06-07 16:28:53 +0900

    Move post args after a rest arg in RBS hover (#209)
    Follow up of #205

--- scenario/service/hover4.rb
+++ scenario/service/hover4.rb
@@ -7,6 +7,8 @@ class Object
   def post_required_positional_args: (?Integer, Integer) -> Integer
 
   def rest_positional_args: (*Integer) -> Integer
+
+  def rest_post_positional_args: (*Integer, Integer) -> Integer
 end
 
 ## update: test.rb
@@ -14,6 +16,7 @@ required_positional_args
 optional_positional_args
 post_required_positional_args
 rest_positional_args
+rest_post_positional_args
 
 ## hover: test.rb:1:1
 Object#required_positional_args : (::Integer) -> ::Integer
@@ -26,3 +29,6 @@ Object#post_required_positional_args : (?::Integer, ::Integer) -> ::Integer
 
 ## hover: test.rb:4:1
 Object#rest_positional_args : (*::Integer) -> ::Integer
+
+## hover: test.rb:5:1
+Object#rest_post_positional_args : (*::Integer, ::Integer) -> ::Integer
