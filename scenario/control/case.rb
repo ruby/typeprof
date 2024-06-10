441e60e0d154f200a926224e96a0852339b64e71
alpaca-tc <alpaca-tc@alpaca.tc>
2024-06-10 11:21:23 +0900

    Fix an error when parsing `case` statement without predicate

--- scenario/control/case.rb
+++ scenario/control/case.rb
@@ -38,10 +38,18 @@ def qux(n)
   end
 end
 
+def without_predicate(n)
+  case
+  when true
+    1
+  end
+end
+
 ## assert
 class Object
   def foo: (untyped) -> (Float | Integer | String)
   def bar: (untyped) -> (Integer | String)?
   def baz: (untyped) -> (Integer | String)
   def qux: (untyped) -> nil
+  def without_predicate: (untyped) -> Integer?
 end
