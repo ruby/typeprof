517b0281669682867efe6b77c772dcb71cc0bbcb
alpaca-tc <alpaca-tc@alpaca.tc>
2024-06-17 12:36:52 +0900

    Support singleton class node

--- scenario/class/dynamic.rb
+++ scenario/class/dynamic.rb
@@ -8,3 +8,13 @@ end
 ## diagnostics
 (1,0)-(2,3): TypeProf cannot analyze a non-static module
 (4,0)-(5,3): TypeProf cannot analyze a non-static class
+
+## update
+Class.new do
+  class << self
+    def foo = :ok
+  end
+end
+
+## diagnostics
+(2,2)-(4,5): TypeProf cannot analyze a non-static class
