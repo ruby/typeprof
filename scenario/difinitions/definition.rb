b09976c72305fe8d5992b6210b3b022d07bf9d22
NotFounds(Iori IKEDA) <NotFounds8080@gmail.com>
2024-06-21 23:07:09 +0900

    Add tests or definitions

---
+++ scenario/difinitions/definition.rb
@@ -0,0 +1,20 @@
+## update: test.rb
+module Animal
+  class Cat
+    def call
+      puts "meow"
+    end
+  end
+end
+
+Animal::Cat.new.call
+
+## definition: test.rb:9:1
+test.rb:(1,7)-(1,13)
+test.rb:(2,8)-(2,11)
+
+## definition: test.rb:9:9
+test.rb:(2,8)-(2,11)
+
+## definition: test.rb:9:17
+test.rb:(3,8)-(3,12)
--- scenario/difinitions/definition_multibyte_characters.rb
+++ scenario/difinitions/definition_multibyte_characters.rb
@@ -1,20 +1,19 @@
 ## update: test.rb
 module A動物
   class B猫
-    def call
+    def 🐱🐱🐱
       puts "にゃー"
     end
   end
 end
 
-A動物::B猫
-A動物::B猫.new.call
+A動物::B猫.new.🐱🐱🐱
 
 ## definition: test.rb:9:1
 test.rb:(1,7)-(1,10) # Jump to module
 
-## definition: test.rb:10:6
+## definition: test.rb:9:6
 test.rb:(2,8)-(2,10) # Jump to class
 
-## definition: test.rb:10:13
-test.rb:(3,8)-(3,12) # Jump to #call method
+## definition: test.rb:9:13
+test.rb:(3,8)-(3,14) # Jump to #call method
