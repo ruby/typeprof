772ad2f2c612e291f3820ab815b49de8ed706fa5
Yusuke Endoh <mame@ruby-lang.org>
2024-07-22 18:29:26 +0900

    Add a pedantic case

---
+++ scenario/class/self-in-cbase.rb
@@ -0,0 +1,20 @@
+## update
+class Foo
+  class self::Bar::Baz
+  end
+end
+
+## assert
+class Foo
+  class Foo::Bar::Baz
+  end
+end
+
+## update
+class self::X
+end
+
+## assert
+
+## diagnostics
+(1,0)-(2,3): TypeProf cannot analyze a non-static class
