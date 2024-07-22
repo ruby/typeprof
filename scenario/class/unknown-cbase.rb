0c0c59fd1d827b282a28f2f88d8a88c641c7f15b
Yusuke Endoh <mame@ruby-lang.org>
2024-07-22 15:16:10 +0900

    Add an rbs for TypeProf namespace

---
+++ scenario/class/unknown-cbase.rb
@@ -0,0 +1,37 @@
+## update: test.rb
+class Foo::Bar
+  class C
+    def foo
+      1
+    end
+  end
+end
+
+# Currently, Foo::Bar::C cannot be resolved because there is no definition of Foo
+class D < Foo::Bar::C
+  def check = foo
+end
+
+## assert: test.rb
+class Foo::Bar
+  class Foo::Bar::C
+    def foo: -> Integer
+  end
+end
+class D # failed to identify its superclass
+  def check: -> untyped
+end
+
+## update: test.rbs
+module Foo
+end
+
+## assert: test.rb
+class Foo::Bar
+  class Foo::Bar::C
+    def foo: -> Integer
+  end
+end
+class D < Foo::Bar::C
+  def check: -> Integer
+end
