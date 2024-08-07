330e12351784aae78b6c9b34fd86775c483233dc
Yusuke Endoh <mame@ruby-lang.org>
2024-08-07 15:59:32 +0900

    Tentative support of for node

---
+++ scenario/misc/for.rb
@@ -0,0 +1,19 @@
+## update
+def foo
+  for i in 1..100
+    # currently, TypeProf does not initialize i
+    return 1
+  end
+end
+
+def bar
+  for i in 1..100
+    return 1
+  end
+end
+
+## assert
+class Object
+  def foo: -> Integer?
+  def bar: -> Integer?
+end
