ed61352ab98bd5a9185597befc37d5de3369d53c
Yusuke Endoh <mame@ruby-lang.org>
2024-08-23 11:01:42 +0900

    Temporarily ignore "return" in non-method scope
    Currently TypeProf does not support `define_method`. A return statement
    in `define_method` is considered as a non-method-scope return, which is
    not well supported. This change temporarily ignores such a case.

--- lib/typeprof/core/ast/control.rb
+++ lib/typeprof/core/ast/control.rb
@@ -313,8 +313,8 @@ module TypeProf::Core
 
       def install0(genv)
         @arg.install(genv)
-        e_ret = @lenv.get_var(:"*expected_method_ret")
-        @lenv.add_return_box(@changes.add_escape_box(genv, @arg.ret, e_ret))
+        e_ret = @lenv.locals[:"*expected_method_ret"]
+        @lenv.add_return_box(@changes.add_escape_box(genv, @arg.ret, e_ret)) if e_ret
         Source.new(Type::Bot.new(genv))
       end
     end
