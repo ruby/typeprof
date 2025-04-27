f15973554fb66db089dc6aba93ff72c1e3263705
Masato Sugiyama <public@smasato.net>
2025-04-27 20:52:43 +0900

    Add test scenario for splat operator in rescue clauses

--- scenario/control/rescue-assign.rb
+++ scenario/control/rescue-assign.rb
@@ -6,11 +6,20 @@ rescue StandardError => e
   e.message
 end
 
+def bar(n)
+  raise if n != 0
+  n.to_s
+rescue *[StandardError] => e
+  e.message
+end
+
 foo(1)
+bar(1)
 
 ## diagnostics
 
 ## assert
 class Object
   def foo: (Integer) -> String
+  def bar: (Integer) -> String
 end
