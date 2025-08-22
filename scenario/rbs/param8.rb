c04caec2011213ed9703a84389fd1e9d240783b8
Yusuke Endoh <mame@ruby-lang.org>
2025-08-22 18:19:29 +0900

    Enhance scenario/rbs/param8.rb

--- scenario/rbs/param8.rb
+++ scenario/rbs/param8.rb
@@ -1,4 +1,33 @@
 ## update: test.rbs
 class Foo[X]
+  def initialize: (X) -> void
   @x: X
+  # TODO: Currently, handling a type variable is immature.
+  # Unless the explicit declaration of get_x, a bare type variable X is leaked.
+  # A naive fix will bring huge performance penalties, so we need a good solution.
+  def get_x: -> X
+end
+
+## update: test.rb
+class Foo
+  def initialize(x)
+    @x = x
+  end
+
+  def get_x
+    @x
+  end
+end
+
+def check
+  Foo.new(1).get_x
+end
+
+## assert
+class Foo
+  def initialize: (var[X]) -> (Object | var[X])
+  def get_x: -> var[X]
+end
+class Object
+  def check: -> Integer
 end
