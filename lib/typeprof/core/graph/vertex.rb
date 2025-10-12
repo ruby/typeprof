04982fcb3294735d9af8f3b2c8396a566cbc44ee
Takumi Shotoku <sinsoku.listy@gmail.com>
2025-10-12 12:23:51 +0900

    Fix RuntimeError when using multiple hash splat operators
    When multiple hash splat operators expand hashes of the same type,
    duplicate edges were being added to the same vertex, causing `Set#<<`
    to raise an error. Modified `Vertex#add_edge` and `Vertex#remove_edge`
    to handle duplicate edges gracefully.

--- lib/typeprof/core/graph/vertex.rb
+++ lib/typeprof/core/graph/vertex.rb
@@ -188,12 +188,14 @@ module TypeProf::Core
     end
 
     def add_edge(genv, nvtx)
+      return if @next_vtxs.include?(nvtx)
       @next_vtxs << nvtx
       nvtx.on_type_added(genv, self, @types.keys) unless @types.empty?
     end
 
     def remove_edge(genv, nvtx)
-      @next_vtxs.delete(nvtx) || raise
+      return unless @next_vtxs.include?(nvtx)
+      @next_vtxs.delete(nvtx)
       nvtx.on_type_removed(genv, self, @types.keys) unless @types.empty?
     end
 
