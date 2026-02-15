93ea24693ad8f1fd1edbe0f5fc824ddd0cb3d247
whtsht <whiteshirt0079@gmail.com>
2026-02-15 22:38:26 +0900

    Add new rbs-inline hover scenario

--- scenario/rbs/inline-hover.rb
+++ scenario/rbs/inline-hover.rb
@@ -9,3 +9,17 @@ Integer
 
 ## hover: test.rb:3:3
 Integer
+
+## update: test2.rb
+class Foo
+  #: (String) -> void
+  def bar(x)
+    x
+  end
+end
+
+## hover: test2.rb:3:10
+String
+
+## hover: test2.rb:4:4
+String
