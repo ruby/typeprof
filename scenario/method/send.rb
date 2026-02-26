c8647d420ab3e7548c7f80e2408100085dad7f6b
Yusuke Endoh <mame@ruby-lang.org>
2026-02-26 08:09:43 +0000

    Support splat arguments in Kernel#send builtin
    Handle the case where send receives a splatted array (e.g., send(*[:foo, 1])).
    For tuple arrays, extract the first element as the method name and remaining
    elements as arguments with full per-element precision. For non-tuple arrays,
    use the unified element type as the method name.

--- scenario/method/send.rb
+++ scenario/method/send.rb
@@ -71,3 +71,73 @@ class Foo4
   def bar: (Integer) -> String
   def test: -> Array[:bar | :foo]
 end
+
+## update
+class Foo5
+  def bar(n)
+    n
+  end
+
+  def test
+    send(*[:bar, 1])
+  end
+end
+
+## assert
+class Foo5
+  def bar: (Integer) -> Integer
+  def test: -> Integer
+end
+
+## update
+class Foo6
+  def bar(a, b)
+    a
+  end
+
+  def test
+    send(*[:bar], 1, 2)
+  end
+end
+
+## assert
+class Foo6
+  def bar: (Integer, Integer) -> Integer
+  def test: -> Integer
+end
+
+## update
+class Foo7
+  def foo(n)
+    n
+  end
+
+  def test
+    ary = []
+    ary << :foo
+    send(*ary, 1)
+  end
+end
+
+## assert
+class Foo7
+  def foo: (Integer) -> Integer
+  def test: -> Integer
+end
+
+## update
+class Foo8
+  def bar(a, b)
+    a
+  end
+
+  def test
+    send(*[:bar, 1, 2].to_a)
+  end
+end
+
+## assert
+class Foo8
+  def bar: (untyped, untyped) -> untyped
+  def test: -> untyped
+end
